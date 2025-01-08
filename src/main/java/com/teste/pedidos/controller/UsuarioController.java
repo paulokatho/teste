bb9c5c75e0f1f38bac1add24b070c5b760e19800
paulokatho <pkatho@gmail.com>
2025-01-08 11:21:47 +0000

    feat: Implementação das classes de controller

---
+++ src/main/java/com/teste/pedidos/controller/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.teste.pedidos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.teste.pedidos.services.UsuarioService;
+
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService service;
+	
+}
