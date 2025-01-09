f6ccb39edd68af6f8b61bc6fcb40a12fe7df208a
paulokatho <pkatho@gmail.com>
2025-01-09 08:58:12 +0000

    feat: Excluindo busca por nome do artigo

--- src/main/java/com/teste/pedidos/repositories/ArtigoRepository.java
+++ src/main/java/com/teste/pedidos/repositories/ArtigoRepository.java
@@ -8,8 +8,6 @@ import com.teste.pedidos.entities.Artigo;
 
 public interface ArtigoRepository extends JpaRepository<Artigo, Long> {
 	
-	Optional<Artigo> findByNome(String nome);
-
 	Optional<Artigo> findById(Long id);
 
 }
